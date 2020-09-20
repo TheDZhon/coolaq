09f11c73e0619efeb74f3b21111f84759c9eeb45
Eugene Mamin <TheDZhon@gmail.com>
2020-09-20 17:00:04 +0300

    Initial commit
    Create project structure.
    Various configuration files and presets.

---
+++ src/main.cpp
@@ -0,0 +1,56 @@
+#include <TFT_eSPI.h>
+#include <SPI.h>
+
+#include <Button2.h>
+
+#ifndef TFT_DISPOFF
+#define TFT_DISPOFF 0x28
+#endif
+
+#ifndef TFT_SLPIN
+#define TFT_SLPIN   0x10
+#endif
+
+#define TFT_MOSI            19
+#define TFT_SCLK            18
+#define TFT_CS              5
+#define TFT_DC              16
+#define TFT_RST             23
+
+#define TFT_BL              4   // Display backlight control pin
+#define ADC_EN              14  //ADC_EN is the ADC detection enable port
+#define ADC_PIN             34
+#define BUTTON_1            35
+#define BUTTON_2            0
+
+TFT_eSPI tft = TFT_eSPI(135, 240); // Invoke custom library
+
+void setup ()
+{
+    Serial.begin(9600);
+    Serial.println("Start");
+
+    tft.init();
+    tft.setRotation(0);
+    tft.fillScreen(TFT_BLACK);
+    tft.setTextSize(2);
+    tft.setTextColor(TFT_GREEN);
+    tft.setCursor(0, 0);
+    tft.setTextDatum(TL_DATUM);
+    tft.setTextSize(1);   
+
+    if (TFT_BL > 0) 
+    {                           // TFT_BL has been set in the TFT_eSPI library in the User Setup file TTGO_T_Display.h
+        pinMode(TFT_BL, OUTPUT);                // Set backlight pin to output mode
+        digitalWrite(TFT_BL, TFT_BACKLIGHT_ON); // Turn backlight on. TFT_BACKLIGHT_ON has been set in the TFT_eSPI library in the User Setup file TTGO_T_Display.h
+    }
+
+    tft.setSwapBytes(true);
+    
+    tft.drawString ("Init completed", 0, 0);
+}
+
+void loop ()
+{
+    //TODO
+}
